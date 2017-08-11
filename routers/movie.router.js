d9f2d4cd3c53d8440a83503ef87fa07bb5a095a2
Angela Graves <algraves78@gmail.com>
2017-08-11 19:59:07 -0400

    Built the route that allows consumers of this api to send the movie id for a specific movie as part of the url. Built an additional route that allows a user to search by movie title via the url. Added a final route that populates the list of available movie genres

--- routers/movie.router.js
+++ routers/movie.router.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const themoviedb = require('../credentials').themoviedb;
-const baseUrl = `https://api.themoviedb.org/3/movie/`
+const baseUrl = `https://api.themoviedb.org/3/`
 const apikey = `?api_key=${themoviedb}`;
 const axios = require('axios');
 
 router.get('/movies', (request, response) => {
-  const url = `${baseUrl}603${apikey}`
+  const url = `${baseUrl}movie/603${apikey}`;
   axios.get(url)
        .then(movie => {
          response.json(movie.data);
@@ -17,7 +17,41 @@ router.get('/movies', (request, response) => {
 });
 
 router.get('/movies/:movie_id', (request, response) => {
-  response.send('Here are the details for a specific movie');
+  const movie_id = request.params.movie_id;
+  const url = `${baseUrl}movie/${movie_id}${apikey}`;
+  axios.get(url)
+       .then(movie => {
+         response.json(movie.data);
+       })
+       .catch(err => {
+         console.error(err);
+       });
+});
+
+router.get('/search/:movie', (request, response) => {
+  const movie = request.params.movie;
+  const query = `&query=${movie}`;
+  const url = `${baseUrl}search/movie${apikey}${query}`;
+  axios.get(url)
+       .then(movie => {
+         response.json(movie.data);
+       })
+       .catch(err => {
+         console.error(err);
+       });
+
+});
+router.get('/browse/:genres', (request, response) => {
+  const genres = request.params.genres;
+  const url = `${baseUrl}genre/movie/list${apikey}`;
+  axios.get(url)
+       .then(movie => {
+         response.json(movie.data);
+       })
+       .catch(err => {
+         console.error(err);
+       });
+
 });
 
 module.exports = router;
