ded412fc702ec7d25dff35b901011979e6126e14
Angela Graves <algraves78@gmail.com>
2017-08-12 19:46:56 -0400

    added cors middleware to enable cors

--- index.js
+++ index.js
@@ -5,14 +5,16 @@ const port = process.env.PORT || 8080;
 
 // middleware imports
 const morgan = require('morgan');
+const cors = require('cors');
 
 // router imports
 const movieRouter = require('./routers/movie.router');
 
-// wire up the middleware
+// middleware use (wire up the middleware)
+server.use(cors());
 server.use(morgan('dev'));
 
-// wire up the routers
+// router use (wire up the routers)
 server.use(movieRouter);
 
 // dummy route for testing
